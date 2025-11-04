56ede46afe9de44ef647fc707d81a6a0b9d8429a
KartikVerma39 <kartikverma1074@gmail.com>
2025-11-04 15:05:00 +0530

    graphs_basics

--- Graphs/graphs.cpp
+++ Graphs/graphs.cpp
@@ -18,17 +18,140 @@
 
 // List
 
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     int n,m;
+//     cin>>n>>m;
+//     vector<int>adj[n+1];
+//     for(int i = 0 ; i<m ; i++){
+//         int u,v;
+//         cin>>u>>v;
+//         adj[u].push_back(v);
+//         adj[v].push_back(u);
+//     }
+// } 
+
+
+
+
+// BFS in C++
+
+
+// #include<iostream>
+// #include<vector>
+// #include<queue>
+// using namespace std;
+
+// class Graph{
+//     int vertices;
+//     vector<vector<int>>adj;
+
+// public:
+//         Graph(int v){
+//             vertices = v;
+//             adj.resize(v);
+//         }
+
+//         void addEdge(int u , int v){
+//             adj[u].push_back(v);
+//             adj[v].push_back(u);
+//         }
+
+//         void BFS(int start){
+//             vector<bool>visited(vertices , false);
+//             queue<int>q;
+
+//             visited[start] = true;
+//             q.push(start);
+
+//             cout<<"BFS starting from node "<<start<<": ";
+
+//             while(!q.empty()){
+//                 int node = q.front();
+//                 q.pop();
+//                 cout<<node<<" ";
+
+//                 for(int neighbour : adj[node]){
+//                     if(!visited[neighbour]){
+//                         visited[neighbour] = true;
+//                         q.push(neighbour);
+//                     }
+//                 }
+//             }
+//             cout<<endl;
+//         }
+// };
+
+// int main(){
+//     Graph g(6);
+//     g.addEdge(0, 1);
+//     g.addEdge(0, 2);
+//     g.addEdge(1, 3);
+//     g.addEdge(2, 4);
+//     g.addEdge(3, 5);
+
+//     g.BFS(0);
+
+//     return 0;
+// }
+
+
+
+
+
+// DFS in C++
+
+
+
 #include<iostream>
 #include<vector>
 using namespace std;
-int main(){
-    int n,m;
-    cin>>n>>m;
-    vector<int>adj[n+1];
-    for(int i = 0 ; i<m ; i++){
-        int u,v;
-        cin>>u>>v;
+
+class Graph{
+    int V;
+    vector<vector<int>>adj;
+
+public:
+    Graph(int V){
+        this->V = V;
+        adj.resize(V);
+    }
+
+    void addEdge(int u , int v){
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
+
+    void dfsHelper(int node,  vector<bool>& visited){
+        visited[node] = true;
+        cout<<node<<" ";
+
+        for(int neighbour : adj[node]){
+            if(!visited[neighbour]){
+                dfsHelper(neighbour , visited);
+            }
+        }
+    }
+
+    void DFS(int start){
+        vector<bool>visited(V, false);
+        dfsHelper(start , visited);
+    }
+};
+
+int main() {
+    Graph g(6);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
+    g.addEdge(4, 5);
+
+    cout << "DFS starting from node 0: ";
+    g.DFS(0);
+    cout << endl;
+
+    return 0;
 }
