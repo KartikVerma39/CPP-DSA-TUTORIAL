1ca76474c56b690671f308213a372594f4952d87
KartikVerma39 <kartikverma1074@gmail.com>
2025-11-25 11:24:39 +0530

    array_dsa

--- Heap/heap_lecture1.cpp
+++ Heap/heap_lecture1.cpp
@@ -134,8 +134,8 @@
 
 
 
-
 #include<iostream>
+#include<queue>
 using namespace std;
 
 class heap{
@@ -269,4 +269,37 @@ int main(){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
+
+    // maxheap
+
+
+    priority_queue<int>pq;
+
+    pq.push(4);
+    pq.push(2);
+    pq.push(5);
+    pq.push(3);
+
+    cout<<"Element at Top "<<pq.top()<<endl;
+    pq.pop();
+    cout<<"Element at Top "<<pq.top()<<endl;    
+
+    cout<<"Size "<<pq.size()<<endl;
+
+
+    // minheap
+
+    priority_queue<int,vector<int>,greater<int>> minheap;
+
+    minheap.push(4);
+    minheap.push(2);
+    minheap.push(5);
+    minheap.push(3);
+
+    cout<<"Element at Top "<<minheap.top()<<endl;
+    minheap.pop();
+    cout<<"Element at Top "<<minheap.top()<<endl;    
+
+    cout<<"Size "<<minheap.size();
+
 }
