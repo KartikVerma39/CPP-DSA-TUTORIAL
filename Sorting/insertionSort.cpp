69415bc1acccc42dd1d0c08ce1086d74d7d960cd
KartikVerma39 <kartikverma1074@gmail.com>
2025-11-26 12:28:29 +0530

    sorting_dsa

--- Sorting/insertionSort.cpp
+++ Sorting/insertionSort.cpp
@@ -2,17 +2,42 @@
 
 
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int size;
+//     cin>>size;
+//     int arr[size];
+//     for(int i = 0 ; i < size ; i++){
+//         cin>>arr[i];
+//     }
+
+//     for(int i = 1 ; i <= size - 1; i++){
+//         int j = i;
+//         while(j>0 && arr[j-1] > arr[j]){
+//             swap(arr[j] , arr[j-1]);
+//             j--;
+//         }
+//     }
+
+//     for(int i = 0 ; i < size ; i++){
+//         cout<<arr[i]<<" "; 
+//     }
+// }
+
+
+
 #include<iostream>
 using namespace std;
 int main(){
-    int size;
+    int size; 
     cin>>size;
     int arr[size];
     for(int i = 0 ; i < size ; i++){
         cin>>arr[i];
     }
 
-    for(int i = 0 ; i < size - 1; i++){
+    for(int i = 1 ; i <= size - 1 ; i++){
         int j = i;
         while(j>0 && arr[j-1] > arr[j]){
             swap(arr[j] , arr[j-1]);
@@ -20,7 +45,9 @@ int main(){
         }
     }
 
+
     for(int i = 0 ; i < size ; i++){
-        cout<<arr[i]<<" "; 
+        cout<<arr[i]<<" ";
     }
+
 }
