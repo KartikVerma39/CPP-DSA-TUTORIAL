cf637397f07005a08330c75e922cbcbbcb6edcc1
KartikVerma39 <kartikverma1074@gmail.com>
2025-11-26 12:06:21 +0530

    sorting_dsa

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -1,6 +1,38 @@
 // Bubble Sort
 
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int size;
+//     cin>>size;
+//     int arr[size];
+//     for(int i = 0 ; i < size ; i++){
+//         cin>>arr[i];
+//     }
+
+//     for(int i = size - 1 ; i >= 1 ; i--){
+//         int didSwap = 0;
+//         for(int j = 0 ; j<=i-1 ; j++){
+//             if(arr[j] > arr[j+1]){
+//                 swap(arr[j] , arr[j+1]);
+//                 didSwap = 1;
+//             }
+//         }
+//         if(didSwap == 0){
+//             break;
+//         }
+//     }
+
+
+//     for(int i = 0 ;  i<size ; i++){
+//         cout<<arr[i]<<" ";
+//     }
+// }
+
+
+
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -12,20 +44,20 @@ int main(){
     }
 
     for(int i = size - 1 ; i >= 1 ; i--){
-        int didSwap = 0;
-        for(int j = 0 ; j<=i-1 ; j++){
+        int check = 0;
+        for(int j = 0 ; j <= i-1 ; j++){
             if(arr[j] > arr[j+1]){
-                swap(arr[j] , arr[j+1]);
-                didSwap = 1;
+                swap(arr[j],arr[j+1]);
+                check = 1;
             }
         }
-        if(didSwap == 0){
+        if(check == 0){
             break;
         }
     }
 
 
-    for(int i = 0 ;  i<size ; i++){
+    for(int i = 0 ; i < size ; i++){
         cout<<arr[i]<<" ";
     }
 }
